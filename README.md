3ec146951cd3cad075591e0a09a0e302a6dd41c3
Paul Seiffert <paul.seiffert@gmail.com>
2013-06-03 02:16:04 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-require-jsx
-===========
+# require-jsx
 
-A require.js plugin that allows you to use JSX files as require.js dependencies.
+A [RequireJS](http://requirejs.org) plugin for JavaScript files containing [JSX](http://facebook.github.io/react/docs/syntax.html).
+
+It can be used to load JS source file that contain [JSX](http://facebook.github.io/react/docs/syntax.html) code.
+This is helpful when using [React](http://facebook.github.io/react/index.html) with [RequireJS](http://requirejs.org).
+
+## Install <a name="install"></a>
+
+Download the plugin [here](https://raw.github.com/seiffert/require-jsx/master/extras/jsx.js)
+
+Place this in the directory that is your
+[baseUrl](http://requirejs.org/docs/api.html#config-baseUrl) for your project,
+or set up a [paths config](http://requirejs.org/docs/api.html#config-paths)
+for it for the module ID `jsx`.
+
+## Usage <a name="usage".</a>
+
+First, you need to configure RequireJS to use Facebook's [JSXTransformer](http://fb.me/JSXTransformer-0.3.0.js) which also is 
+in [React](http://facebook.github.io/react/index.html):
+
+    require.config({
+        deps: ["main"],
+        
+        paths: {
+            jsx: "../lib/jsx",
+            JSXTransformer: '../lib/JSXTransformer'
+        },
+        
+        shim: {
+            JSXTransformer: {
+                exports: "JSXTransformer"
+            }
+        }
+    });
+
+Then, you can reference JSX files via the `jsx!` plugin syntax. For example, to load
+the `router.js` file that is in your `app` directory:
+
+    require(['jsx!app/router'], function (Router) {
+
+    });
---
+++ jsx.js
@@ -0,0 +1,75 @@
+define(['JSXTransformer', 'react'], function (JSXTransformer, react) {
+    'use strict';
+    
+    var fs, getXhr,
+        progIds = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.4.0'],
+        fetchText = function () {
+            throw new Error('Environment unsupported.');
+        },
+        buildMap = {};
+ 
+    getXhr = function () {
+        var xhr, i, progId;
+        if (typeof XMLHttpRequest !== "undefined") {
+            return new XMLHttpRequest();
+        } else {
+            for (i = 0; i < 3; i += 1) {
+                progId = progIds[i];
+                try {
+                    xhr = new ActiveXObject(progId);
+                } catch (e) {}
+
+                if (xhr) {
+                    progIds = [progId];  // so faster next time
+                    break;
+                }
+            }
+        }
+
+        if (!xhr) {
+            throw new Error("getXhr(): XMLHttpRequest not available");
+        }
+
+        return xhr;
+    };
+
+    fetchText = function (url, callback) {
+        var xhr = getXhr();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function (evt) {
+            if (xhr.readyState === 4) {
+                callback(xhr.responseText);
+            }
+        };
+        xhr.send(null);
+    };
+ 
+    return {
+        write: function (pluginName, name, write) {
+            if (buildMap.hasOwnProperty(name)) {
+                var text = buildMap[name];
+                write.asModule(pluginName + "!" + name, text);
+            }
+        },
+ 
+        version: '0.1',
+ 
+        load: function (name, parentRequire, load, config) {
+            var path = parentRequire.toUrl(/\.(js)$/.test(name) ? name : name + '.js');
+            fetchText(path, function (text) {
+                try {
+                    if (-1 === text.indexOf('React.DOM')) {
+                        text = '/** @jsx React.DOM */' + text;
+                    }
+                    
+                    text = JSXTransformer.transform(text).code;
+                } catch (err) {
+                    err.message = "In " + path + ", " + err.message;
+                    throw err;
+                }
+ 
+                load.fromText(text);
+            });
+        }
+    };
+});
